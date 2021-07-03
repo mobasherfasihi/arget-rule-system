458e6fd7bfb24157a1148940292ceb22b480fd38
Mobasher Fasihy <mobasher@netlinks.af>
2021-07-03 14:10:54 +0430

    Fix issues of task.js

--- public/js/task.js
+++ public/js/task.js
@@ -5,12 +5,10 @@ const id = new URLSearchParams(
 
 
 window.onload = function() {
-    console.log({self}, {id})
     $.ajax({
         url: `http://ec2-13-235-95-181.ap-south-1.compute.amazonaws.com/api/page-target/${id}/pattern`,
         context: document.body,
         success: function(response) {
-            console.log("heyyyyyyyyyy", response, response.data);
             let url = location.href;
             if (response.target_rule && url.match(response.target_rule)) {
                 if (response.alert_message) alert(response.alert_message);
